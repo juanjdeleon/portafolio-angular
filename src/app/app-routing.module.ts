dda3e135e904e5c84900f408944d08d708d8a524
Juan De Leon <newjuan@gmail.com>
2018-09-21 11:16:30 -0500

    Agregar hash a las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const app_routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot (app_routes)
+        RouterModule.forRoot (app_routes, {useHash: true})
     ],
     exports: [
         RouterModule
