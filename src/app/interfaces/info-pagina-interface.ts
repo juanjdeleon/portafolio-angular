703a20fd118a7475ae2799a515be1439503e4c06
Juan De Leon <newjuan@gmail.com>
2018-09-24 11:27:31 -0500

    Activar servicio para recopilar datos a traves de JSON

---
+++ src/app/interfaces/info-pagina-interface.ts
@@ -0,0 +1,7 @@
+export interface InfoPaginaInterface {
+  nombre?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPaginaInterface } from '../interfaces/info-pagina-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
+  info: InfoPaginaInterface = {};
+
   constructor(private http: HttpClient) {
     http.get('assets/data/data-pagina.json')
-      .subscribe(resp => {
-        console.log(resp);
+      .subscribe(result => {
+        this.info = result;
       });
     console.log('Servido, joven.');
   }
